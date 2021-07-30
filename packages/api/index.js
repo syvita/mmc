d668d5ed69dcc08838ef07d562bb17ada484365d
asteria <asteria@syvita.org>
2021-07-30 18:20:32 +0100

    add

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,4 +1,5 @@
 import { Router } from 'itty-router'
+import { withContent } from 'itty-router-extras'
 
 // Create a new router
 const router = Router()
@@ -7,21 +8,32 @@ router.get("/", () => {
   return new Response("go back to the main page smh")
 })
 
-// POST: create value
+// PUT: create value
 
-router.post("/:key", ({ params }) => {
-  let key = decodeURIComponent(params.key)
+router.put("/:key", withContent, async ({ content, params }) => {
+  let key = decodeURIComponent(params.key).toString().toLowerCase()
+  let keyLength = key.length
+  if (keyLength != 64) {
+    return Response("key length not 64", { status: 403 })
+  } else {
+    await KV.put(JSON.stringify(content))
+  }
 })
+
+// GET: https://api.miamicoin.com/2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
+// key must be 64 characters as a string
+// 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
 
 // GET: get value
-router.get("/:key", ({ params }) => {
-  let key = decodeURIComponent(params.key)
-})
-
-// UPDATE: update value
-
-router.update("/:key", ({ params }) => {
-  let key = decodeURIComponent(params.key)
+router.get("/:key", async ({ params }) => {
+  let key = decodeURIComponent(params.key).toString().toLowerCase()
+  let keyLength = key.length
+  if (keyLength != 64) {
+    return Response("key length not 64", { status: 403 })
+  } else {
+    const value = await KV.get(key)
+    return Response(value)
+  }
 })
 
 router.all("*", () => new Response("404, not found!", { status: 404 }))
