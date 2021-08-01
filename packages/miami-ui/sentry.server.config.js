820c3306baf6bc0e854ddf41b0960b2764472423
asteria <asteria@syvita.org>
2021-08-01 20:27:51 +0100

    adds release versioning for sentry

--- packages/miami-ui/sentry.server.config.js
+++ packages/miami-ui/sentry.server.config.js
@@ -4,14 +4,9 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN || "https://6c2a0bedac06463b8fea519e8603f873@logs.syvita.org/3",
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
   tracesSampleRate: 1.0,
+  release: process.env.npm_package_version,
   sendDefaultPii: false,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 });
 
 // this acts as a safeguard to ensure everyone can prove that we are not 
