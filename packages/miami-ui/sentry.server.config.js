1a529c6e547e59b65c9901ae3b7965bd5b5480e6
asteria <asteria@syvita.org>
2021-08-01 14:53:24 +0100

    please remove IPs

--- packages/miami-ui/sentry.server.config.js
+++ packages/miami-ui/sentry.server.config.js
@@ -7,6 +7,7 @@ Sentry.init({
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
+  sendDefaultPii: false,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
