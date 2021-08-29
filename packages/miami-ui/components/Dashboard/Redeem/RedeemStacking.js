9c234b0fa9c2cb5685fef6f35982ace107cd5d18
SyInvidia <paradinight@protonmail.com>
2021-08-29 19:53:05 +0100

    get-current-cycle

--- packages/miami-ui/components/Dashboard/Redeem/RedeemStacking.js
+++ packages/miami-ui/components/Dashboard/Redeem/RedeemStacking.js
@@ -1,36 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../../../styles/RedeemStacking.module.css';
 import { useAtom } from "jotai";
 import { userSessionState } from "../../../lib/auth";
-import fetch from "cross-fetch";
-import { Configuration, AccountsApi } from "@stacks/blockchain-api-client";
+import { getUserId, getStackingRewardForCycle, getCurrentCycle } from "../../../lib/contracts";
 import {
   NETWORK_STRING,
   CITY_COIN_CORE_ADDRESS,
   CITY_COIN_CORE_CONTRACT_NAME,
   NETWORK,
 } from "../../../lib/constants";
-import {
-  canClaimMiningReward,
-} from "../../../lib/contracts";
 import { useConnect } from "@syvita/connect-react";
-import { uintCV } from "@syvita/transactions";
+import { uintCV, standardPrincipalCV } from "@syvita/transactions";
 
 const RedeemStacking = () => {
     const prevCycle = 1;
     const [cycleToRedeem, setCycleToRedeem] = useState(1);
     const [userSession] = useAtom(userSessionState);
-
+    const [userId, setUserId] = useState(0);
+    const [currentCycle, setCurrentCycle] = useState(0);
+    const userData = userSession.loadUserData();
     const { doContractCall } = useConnect();
+
+
+    let STXAddress = '';
+
+    if (NETWORK_STRING == "mainnet") {
+        STXAddress = userData.profile.stxAddress.mainnet;
+      } else {
+        STXAddress = userData.profile.stxAddress.testnet;
+    }
+
+    useEffect(() => {
+        getUserId(STXAddress).then(result => setUserId(result))
+        getCurrentCycle().then(result => setCurrentCycle(result))
+    }, [])
 
     function redeemCycleRewards() {
         if (!(cycleToRedeem > 0)) {
             setCycleToRedeem(1);
         }
-        claimAction(cycleToRedeem);
+        claimStackingReward(cycleToRedeem);
     }
 
-    async function claimAction(cycleToRedeem) {
+    async function claimStackingReward(cycleToRedeem) {
         await doContractCall({
           contractAddress: CITY_COIN_CORE_ADDRESS,
           contractName: CITY_COIN_CORE_CONTRACT_NAME,
@@ -41,13 +53,16 @@ const RedeemStacking = () => {
             setTxId(result.txId);
           },
         });
-      }
+    }
 
     return (
         <div className={styles.redeemStacking}>
         <h2 className={styles.h2}>Redeem stacking rewards</h2>
         <p>
-          Claim STX rewards from your stacked $MIA at the end of each cycle. Please enter a cycle number to redeem $MIA for. If you don't enter a cycle, it will check the previous cycle for rewards.
+          You have 56,889 STX from cycles 8-13. Send the transactions below to redeem them.
+        </p>
+        <p>
+          You'll need to send a transaction for every cycle.
         </p>
         Cycle:
         <input
@@ -58,7 +73,8 @@ const RedeemStacking = () => {
             type="number"
             />
             <button onClick={() => redeemCycleRewards()}>Redeem</button>
-      </div>
+            {currentCycle}
+        </div>
     )
 }
 
